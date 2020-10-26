922dc7befa481e2cc525579f1b9f4ed6163be89f
Nisha Chandramoorthy <nishac@mit.edu>
2020-10-26 13:26:19 -0400

    Updates from Overleaf

--- code/tent_sens_stat/plot_map.py
+++ code/tent_sens_stat/plot_map.py
@@ -38,32 +38,21 @@ def accumulate(nbins, n, m, s):
 nbins = 2**13
 ns = 5
 nn = 5
-n_arr = [0, 1, 2, 4, 6]
-s_arr = [0.005, 0.1, 0.2, 0.4, 0.5]
+n_arr = [0, 2, 4, 6]
+s_arr = [0.2, 0.5]
 
 x = linspace(0, 2, nbins+2)[1:-1]
 ys = zeros((ns,nbins))
 yn = zeros((nn,nbins))
-for k,nk in enumerate(n_arr):
-    for i, xi in enumerate(x):
-        yn[k,i] = osc_tent(xi, 0.5, nk)
-fig = figure(figsize=(8,6))
-ax = fig.add_subplot(111)
-for k,nk in enumerate(n_arr):
-    ax.plot(x, yn[k],lw=2.5,label="n = {}".format(nk))
-ax.xaxis.set_tick_params(labelsize=40)
-ax.yaxis.set_tick_params(labelsize=40)
-leg = ax.legend(fontsize=30)
-ax.axis("scaled")
-
-for k,sk in enumerate(s_arr):
-    for i, xi in enumerate(x):
-        ys[k,i] = osc_tent(xi, sk, 0)
-fig = figure(figsize=(8,6))
-ax = fig.add_subplot(111)
-for k,sk in enumerate(s_arr):
-    ax.plot(x, ys[k],lw=2.5,label="s = {}".format(sk))
-ax.xaxis.set_tick_params(labelsize=40)
-ax.yaxis.set_tick_params(labelsize=40)
-ax.axis("scaled")
-leg = ax.legend(fontsize=30)
+for kn,nk in enumerate(n_arr):
+    for k,sk in enumerate(s_arr):
+        for i, xi in enumerate(x):
+            yn[k,i] = osc_tent(xi, sk, nk)
+    fig = figure(figsize=(8,6))
+    ax = fig.add_subplot(111)
+    for k,sk in enumerate(s_arr):
+        ax.plot(x, yn[k],lw=2.5,label="s = {}".format(sk))
+    ax.xaxis.set_tick_params(labelsize=40)
+    ax.yaxis.set_tick_params(labelsize=40)
+    leg = ax.legend(fontsize=30)
+    ax.axis("scaled")
